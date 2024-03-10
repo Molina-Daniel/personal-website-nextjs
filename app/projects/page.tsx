09483992b8bfde01d0a223a3048e7d668707ad1b
Daniel Molina <danielmolina.dev@gmail.com>
2024-03-10 20:31:30 +0100

    feat: add header

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Projects page content</p>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ components/Nav.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavProps {
+  containerStyles: string;
+  linkStyles: string;
+  underlineStyles: string;
+}
+
+const links = [
+  { name: "home", path: "/" },
+  { name: "projects", path: "/projects" },
+  { name: "contact", path: "/contact" },
+];
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }: NavProps) => {
+  const path = usePathname();
+  return (
+    <nav className={`${containerStyles}`}>
+      {links.map((link, index) => {
+        return (
+          <Link
+            key={index}
+            href={link.path}
+            className={`capitalize ${linkStyles}`}
+          >
+            {link.path === path && (
+              <motion.span
+                initial={{ y: "-100%" }}
+                animate={{ y: 0 }}
+                transition={{ type: "tween" }}
+                layoutId="underline"
+                className={`${underlineStyles}`}
+              />
+            )}
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;
