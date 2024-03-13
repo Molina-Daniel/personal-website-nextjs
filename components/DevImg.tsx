ed614750fe4e77c46dfc3987964a2ff1730a40d3
Daniel Molina <danielmolina.dev@gmail.com>
2024-03-13 10:37:54 +0100

    feat: add hero image

--- components/DevImg.tsx
+++ components/DevImg.tsx
@@ -1,5 +1,11 @@
-const DevImg = () => {
-  return <div>DevImg</div>;
+import Image from "next/image";
+
+const DevImg = ({ containerStyles, imgSrc }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      <Image src={imgSrc} alt="Daniel Molina" fill priority />
+    </div>
+  );
 };
 
 export default DevImg;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { RiArrowDownSLine } from "react-icons/ri";
 import { Download, Send } from "lucide-react";
 import { Button } from "./ui/button";
 import Socials from "./Socials";
+import DevImg from "./DevImg";
 
 const Hero = () => {
   return (
@@ -37,7 +38,13 @@ const Hero = () => {
             />
           </div>
           {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc=""
+            />
+          </div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
