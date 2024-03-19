7d73e00a004cbe08e4ce73b8c6daf9492eddd4a0
Daniel Molina <danielmolina.dev@gmail.com>
2024-03-19 18:23:57 +0100

    feat: add about section

--- components/DevImg.tsx
+++ components/DevImg.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-const DevImg = ({ containerStyles, imgSrc }) => {
+interface DevImgProps {
+  containerStyles: string;
+  imgSrc: string;
+}
+
+const DevImg = ({ containerStyles, imgSrc }: DevImgProps) => {
   return (
     <div className={`${containerStyles}`}>
       <Image src={imgSrc} alt="Daniel Molina" fill priority />
